9a9b096270ee89276df03b74028d02a15aaa78b9
zhangjun123 <lovejjfg@163.com>
2016-04-21 22:42:04 +0800

    基本实现RecycleView的全屏滑动。。

--- app/src/main/java/com/lovejjfg/recycleviewdemo/MainActivity.java
+++ app/src/main/java/com/lovejjfg/recycleviewdemo/MainActivity.java
@@ -1,29 +1,96 @@
 package com.lovejjfg.recycleviewdemo;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowInsets;
+
+import com.lovejjfg.recycleviewdemo.adapter.MyAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int actionBarSize;
+    private Toolbar toolbar;
+    private RecyclerView mRecyclerView;
+
+    @TargetApi(Build.VERSION_CODES.KITKAT_WATCH)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        CoordinatorLayout parent = (CoordinatorLayout) findViewById(R.id.parent);
+        parent.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+//        parent.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+//            @Override
+//            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+//                mRecyclerView.setPadding(mRecyclerView.getPaddingLeft(),
+//                        insets.getSystemWindowInsetTop() + getActionBarSize
+//                                (MainActivity.this),
+//                        mRecyclerView.getPaddingRight() + insets.getSystemWindowInsetRight(), // landscape
+//                        mRecyclerView.getPaddingBottom());
+//                return insets.consumeSystemWindowInsets();
+//            }
+//        });
+        toolbar.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                initHeight();
+            }
+        });
+        GridLayoutManager manager = new GridLayoutManager(this, 3);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(new MyAdapter());
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                Log.e("scroll::::",dy + "");
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+//        initHeight();
+    }
+
+    @NonNull
+    private void initHeight() {
+        Rect frame = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        final int statusBarHeight = frame.top;
+        final int toolbarHeight = toolbar.getHeight();
+        Log.e("statusBarHeight:", statusBarHeight + "");
+        Log.e("toolbarHeight:", toolbarHeight + "");
+        mRecyclerView.post(new Runnable() {
+            @Override
+            public void run() {
+
+                mRecyclerView.setPadding(mRecyclerView.getPaddingLeft(), mRecyclerView.getPaddingTop() +toolbarHeight+500, mRecyclerView.getPaddingRight(), mRecyclerView.getPaddingBottom());
+                mRecyclerView.scrollBy(0, -(toolbarHeight));
             }
         });
     }
@@ -48,5 +115,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static int getActionBarSize(Context context) {
+        if (actionBarSize < 0) {
+            TypedValue value = new TypedValue();
+            context.getTheme().resolveAttribute(android.R.attr.actionBarSize, value, true);
+            actionBarSize = TypedValue.complexToDimensionPixelSize(value.data, context
+                    .getResources().getDisplayMetrics());
+        }
+        return actionBarSize;
     }
 }
